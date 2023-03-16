dad1070cf2514d54d49dbcd1f8e46adaf5b0b2c2
bill <bill80362@gmail.com>
2023-03-16 13:33:41 +0800

    製作中

--- src/views/GoodsColorListView.vue
+++ src/views/GoodsColorListView.vue
@@ -2,18 +2,87 @@
 import {ref, onMounted, onUpdated, h, onBeforeMount} from "vue";
 import {useDataApiStore} from "../stores/api";
 import router from "../router";
+import {NButton} from "naive-ui";
 
 const DataApi = useDataApiStore();
 
 const page = ref(1)
 const pageTotal = ref(10)
+const pagination = ref({
+    page:1,
+    pageSize:pageTotal,
+})
 
+const TableHeader = ref([
+    {
+        title: 'ID',
+        key: 'ID',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '標題',
+        key: 'Title',
+        defaultSortOrder: 'ascend',
+        // sorter: 'default',
+    },
+    {
+        title: '開關',
+        key: 'Status',
+        defaultSortOrder: 'ascend',
+        // sorter: 'default',
+    },
+    {
+        title: '更新',
+        key: 'updated_at',
+        defaultSortOrder: 'ascend',
+        // sorter: 'default',
+    },
+    {
+        title: '建立',
+        key: 'created_at',
+        defaultSortOrder: 'ascend',
+        // sorter: 'default',
+    },
+    {
+        title: '管理',
+        key: 'operate',
+        render (row) {
+            return h(
+                NButton,
+                {
+                    size: 'small',
+                    type: 'primary',
+                    onClick: () => clickUpdate(row.ID)
+                },
+                { default: () => '編輯' }
+            )
+        }
+    },
+])
+const TableData = ref([]);
+
+//
 onBeforeMount(async ()=>{
     DataApi.ColorList = [];
 })
 
 onMounted(async ()=>{
-  await DataApi.apiColorList()
+    await DataApi.apiColorList()
+    //放入資料
+    TableData.value = [];
+    DataApi.ColorList.forEach((row)=>{
+        TableData.value.push(
+            {
+                ID:row.ColorID,
+                Title:row.ColorTitle,
+                Status:row.Status,
+                updated_at:row.updated_at,
+                created_at:row.created_at,
+                operate: "",
+            }
+        );
+    })
 });
 
 const clickUpdate = (id) =>{
@@ -31,6 +100,11 @@ const clickUpdate = (id) =>{
     </template>
     <n-grid :cols="1">
       <n-gi>
+          <n-data-table
+            ref="table"
+            :columns="TableHeader"
+            :data="TableData"
+          />
         <n-table :bordered="false" :single-line="false">
           <thead>
           <tr>
@@ -59,14 +133,14 @@ const clickUpdate = (id) =>{
       </n-gi>
     </n-grid>
 
-    <template #footer>
-      <n-pagination
-          v-model:page="page"
-          :page-count="pageTotal"
-          show-quick-jumper
-          show-size-picker
-      />
-    </template>
+<!--    <template #footer>-->
+<!--      <n-pagination-->
+<!--          v-model:page="page"-->
+<!--          :page-count="pageTotal"-->
+<!--          show-quick-jumper-->
+<!--          show-size-picker-->
+<!--      />-->
+<!--    </template>-->
 
   </n-page-header>
 </template>
