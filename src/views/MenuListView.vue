ee23f58196e4cf3ad0a932e6365523f5cbebb6b9
bill <bill80362@gmail.com>
2023-03-17 13:19:29 +0800

    製作中

--- src/views/MenuListView.vue
+++ src/views/MenuListView.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, onMounted, onUpdated, h, onBeforeMount} from "vue";
+import {h, onBeforeMount, onMounted, ref} from "vue";
 import {useDataApiStore} from "../stores/api";
 import router from "../router";
 import {NButton} from "naive-ui";
@@ -9,125 +9,125 @@ const DataApi = useDataApiStore();
 const page = ref(1)
 const pageTotal = ref(10)
 const pagination = ref({
-  page:1,
-  pageSize:pageTotal,
+    page: 1,
+    pageSize: pageTotal,
 })
 
 const TableHeader = ref([
-  {
-    title: 'ID',
-    key: 'ID',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '大分類',
-    key: 'CategoryTitle',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '標題',
-    key: 'Title',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '開關',
-    key: 'Status',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '排序',
-    key: 'Seq',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '更新',
-    key: 'updated_at',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '建立',
-    key: 'created_at',
-    defaultSortOrder: 'ascend',
-    sorter: 'default',
-  },
-  {
-    title: '管理',
-    key: 'operate',
-    render (row) {
-      return h(
-          NButton,
-          {
-            size: 'small',
-            type: 'primary',
-            onClick: () => clickUpdate(row.ID)
-          },
-          { default: () => '編輯' }
-      )
-    }
-  },
+    {
+        title: 'ID',
+        key: 'ID',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '大分類',
+        key: 'MenuCategoryTitle',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '標題',
+        key: 'Title',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '開關',
+        key: 'Status',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '排序',
+        key: 'Seq',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '更新',
+        key: 'updated_at',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '建立',
+        key: 'created_at',
+        defaultSortOrder: 'ascend',
+        sorter: 'default',
+    },
+    {
+        title: '管理',
+        key: 'operate',
+        render(row) {
+            return h(
+                NButton,
+                {
+                    size: 'small',
+                    type: 'primary',
+                    onClick: () => clickUpdate(row.ID)
+                },
+                {default: () => '編輯'}
+            )
+        }
+    },
 ])
 const TableData = ref([]);
 let MenuCategoryKeyValue = [];
 
 //
-onBeforeMount(async ()=>{
-  DataApi.MenuList = [];
+onBeforeMount(async () => {
+    DataApi.MenuList = [];
 })
 
-onMounted(async ()=>{
-  await DataApi.apiMenuList()
-  await DataApi.apiMenuCategoryList()
-  //製作分類KeyValue
-  MenuCategoryKeyValue = [];
-  DataApi.MenuCategoryList.forEach((item)=>{
-    MenuCategoryKeyValue[item.MenuCategoryID] = item;
-  });
-  //放入資料
-  TableData.value = [];
-  DataApi.MenuList.forEach((row)=>{
-    TableData.value.push(
-        {
-          ID:row.MenuID,
-          Title:row.Title,
-          CategoryTitle:MenuCategoryKeyValue[row.MenuCategoryID].Title,
-          Status:row.Status,
-          Seq:row.Seq,
-          updated_at:row.updated_at,
-          created_at:row.created_at,
-        }
-    );
-  })
+onMounted(async () => {
+    //製作分類KeyValue
+    await DataApi.apiMenuCategoryList()
+    MenuCategoryKeyValue = [];
+    DataApi.MenuCategoryList.forEach((item) => {
+        MenuCategoryKeyValue[item.MenuCategoryID] = item;
+    });
+    //放入資料
+    await DataApi.apiMenuList()
+    TableData.value = [];
+    DataApi.MenuList.forEach((row) => {
+        TableData.value.push(
+            {
+                ID: row.MenuID,
+                Title: row.Title,
+                MenuCategoryTitle: MenuCategoryKeyValue[row.MenuCategoryID].Title,
+                Status: row.Status,
+                Seq: row.Seq,
+                updated_at: row.updated_at,
+                created_at: row.created_at,
+            }
+        );
+    })
 
 });
 
-const clickUpdate = (id) =>{
-  router.push("/menu/update/"+id);
+const clickUpdate = (id) => {
+    router.push("/menu/update/" + id);
 }
 
 </script>
 
 <template>
-  <n-page-header title="小分類管理" :subtitle="'列表、總筆數:'+DataApi.MenuList.length">
-    <template #extra>
-      <n-space>
-        <n-button type="success" @click="clickUpdate(0)">新增</n-button>
-      </n-space>
-    </template>
-    <n-grid :cols="1">
-      <n-gi>
-        <n-data-table
-            ref="table"
-            :columns="TableHeader"
-            :data="TableData"
-        />
-      </n-gi>
-    </n-grid>
+    <n-page-header title="小分類管理" :subtitle="'列表、總筆數:'+DataApi.MenuList.length">
+        <template #extra>
+            <n-space>
+                <n-button type="success" @click="clickUpdate(0)">新增</n-button>
+            </n-space>
+        </template>
+        <n-grid :cols="1">
+            <n-gi>
+                <n-data-table
+                    ref="table"
+                    :columns="TableHeader"
+                    :data="TableData"
+                />
+            </n-gi>
+        </n-grid>
 
-  </n-page-header>
+    </n-page-header>
 </template>
--- src/views/MenuUpdateView.vue
+++ src/views/MenuUpdateView.vue
@@ -9,28 +9,50 @@ const DataApi = useDataApiStore();
 const route = useRoute()
 
 const Data = ref({});
+let MenuCategoryOption = ref([])
 
 onMounted(async () => {
+    //製作分類KeyValue
+    await DataApi.apiMenuCategoryList()
+    MenuCategoryOption.value = [];
+    DataApi.MenuCategoryList.forEach((item) => {
+        MenuCategoryOption.value.push({
+            label: item.Title,
+            value: item.MenuCategoryID,
+        })
+    });
+    //
+    if (route.params.id !== "0") {
+        //修改
+        DataApi.MenuList.forEach((item, index, array) => {
+            if (route.params.id === item.MenuID) {
+                Data.value = item;
+                Data.value.ID = item.MenuID;
+            }
 
-  if (route.params.id !== "0") {
-    //修改
-    DataApi.MenuList.forEach((item, index, array) => {
-      if (route.params.id === item.MenuID) {
-        Data.value = item;
-        Data.value.ID = item.MenuID;
-      }
-
-    })
-  } else {
-    //新增
-    Data.value = {
-      ID: "0",
-      MenuID: "0",
-      Title: "",
-      Seq: "1",
-      Status: "Y",
+        })
+    } else {
+        //新增
+        Data.value = {
+            ID: "0",
+            CategoryID: "0",
+            MenuID: "0",
+            Title: "",
+            Seq: "1",
+            Status: "Y",
+            Content: "",
+            Content2: "",
+            Content3: "",
+            Content4: "",
+            Content5: "",
+            Content6: "",
+            Content7: "",
+            MenuTimeEnd:"",
+            MenuTimeStart:"",
+            Subtitle:"",
+            Image1:"",
+        }
     }
-  }
 
 });
 onUpdated(() => {
@@ -38,33 +60,64 @@ onUpdated(() => {
 })
 
 const handleBack = () => {
-  router.go(-1);
+    router.go(-1);
 }
 
 const handleSubmit = async () => {
-  await DataApi.apiMenuUpdateCreate(Data.value);
-  handleBack();
+    //新增才有用
+    Data.value.CategoryID = Data.value.MenuCategoryID
+    //
+    await DataApi.apiMenuUpdateCreate(Data.value);
+    handleBack();
 }
 
 </script>
 
 <template>
-  <n-page-header title="商品大分類" subtitle="新增" @back="handleBack">
-    <n-card>
-      <n-form>
-        <n-form-item label="標題">
-          <n-input type="text" v-model:value="Data.Title"/>
-        </n-form-item>
+    <n-page-header title="商品大分類" subtitle="新增" @back="handleBack">
+        <n-card>
+            <n-form>
+                <!--新增才有-->
+                <n-form-item label="大分類" v-show="(route.params.id === '0')">
+                <n-select v-model:value="Data.MenuCategoryID" :options="MenuCategoryOption" />
+                </n-form-item>
+                <n-form-item label="標題">
+                    <n-input type="text" v-model:value="Data.Title"/>
+                </n-form-item>
+                <n-form-item label="副標題">
+                    <n-input type="text" v-model:value="Data.Subtitle"/>
+                </n-form-item>
+                <n-form-item label="內文1">
+                    <n-input type="textarea" v-model:value="Data.Content"/>
+                </n-form-item>
+                <n-form-item label="內文2">
+                    <n-input type="textarea" v-model:value="Data.Content2"/>
+                </n-form-item>
+                <n-form-item label="內文3">
+                    <n-input type="textarea" v-model:value="Data.Content3"/>
+                </n-form-item>
+                <n-form-item label="內文4">
+                    <n-input type="textarea" v-model:value="Data.Content4"/>
+                </n-form-item>
+                <n-form-item label="內文5">
+                    <n-input type="textarea" v-model:value="Data.Content5"/>
+                </n-form-item>
+                <n-form-item label="內文6">
+                    <n-input type="textarea" v-model:value="Data.Content6"/>
+                </n-form-item>
+                <n-form-item label="內文7">
+                    <n-input type="textarea" v-model:value="Data.Content7"/>
+                </n-form-item>
 
-        <n-form-item label="狀態">
-          <n-switch checked-value="Y" unchecked-value="N" v-model:value="Data.Status"/>
-        </n-form-item>
-        <n-form-item label="排序">
-          <n-input type="text" v-model:value="Data.Seq"/>
-        </n-form-item>
-        <n-button @click="handleSubmit">送出</n-button>
-      </n-form>
-    </n-card>
+                <n-form-item label="狀態">
+                    <n-switch checked-value="Y" unchecked-value="N" v-model:value="Data.Status"/>
+                </n-form-item>
+                <n-form-item label="排序">
+                    <n-input-number type="text" v-model:value="Data.Seq"/>
+                </n-form-item>
+                <n-button @click="handleSubmit">送出</n-button>
+            </n-form>
+        </n-card>
 
-  </n-page-header>
+    </n-page-header>
 </template>
